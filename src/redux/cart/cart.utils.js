39aa3efbee830a7892c9c65c8915332e91adc816
Zer0SkiLL <retowidmer95@hotmail.com>
2022-03-19 22:11:52 +0100

    lesson 123: added additem and removeitem func to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,26 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    // get cartItem that should be removes (1 piece)
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    // if it is the last one (quantity 1) remove it from the list
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(
+            (cartItem) => cartItem.id !== cartItemToRemove.id
+        );
+    }
+
+    return cartItems.map((cartItem) =>
+        cartItem.id === cartItemToRemove.id
+            ? {
+                  ...cartItem,
+                  quantity: cartItem.quantity - 1,
+              }
+            : cartItem
+    );
+};
